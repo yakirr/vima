cceb327d8dd5b58d23d0ebeeddae51283e0c8d18
Yakir Reshef <yakir@Yakirs-MacBook-Pro.local>
2024-05-13 14:24:42 -0400

    replaced tf version with torch version

--- core.py
+++ core.py
@@ -1,175 +1,177 @@
 import numpy as np
-import tensorflow as tf
+import torch
+from torch import nn, Tensor
+from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import LRScheduler
+import torch.nn.functional as F
+import time
 import pandas as pd
-from tpae import vis
+import os
+from tempfile import TemporaryDirectory
 
-def prepare(patches, patch_meta, sampleid_name='id', frac_train=0.8, n_examples=16):
-    sample_onehot = pd.get_dummies(patch_meta[sampleid_name]).astype('float32')
-    sampleids = sample_onehot.columns
-    patch_meta = pd.concat([patch_meta, sample_onehot], axis=1)
-
-    ind = np.argsort(np.random.randn(patches.shape[0]))
-    train = ind[:int(frac_train*len(ind))]
-    test = ind[len(train):]
-
-    x_train = np.ascontiguousarray(patches[train])
-    sids_train = np.ascontiguousarray(patch_meta.loc[train, sampleids].values)
-    x_test = np.ascontiguousarray(patches[test])
-    sids_test = np.ascontiguousarray(patch_meta.loc[test, sampleids].values)
-
-    x_test_examples = x_test[:n_examples]
-    sids_test_examples = sids_test[:n_examples]
-
-    return patch_meta, sampleids, x_train, sids_train, x_test, sids_test, x_test_examples, sids_test_examples
-
-@tf.keras.utils.register_keras_serializable()
-class AE(tf.keras.Model):
+class CVAE(nn.Module):
     """Convolutional variational autoencoder."""
 
-    def __init__(self, latent_dim, ncolors, nsamples, patchsize):
-        super(AE, self).__init__()
+    def __init__(self, ncolors : int, patch_size : int,
+            latent_dim: int=100, nfilters1: int=256, nfilters2: int=512):
+        super().__init__()
         self.latent_dim = latent_dim
-        self.encoder = tf.keras.Sequential(
-            [
-            tf.keras.layers.InputLayer(input_shape=(patchsize, patchsize, ncolors)),
-            tf.keras.layers.Conv2D(
-                filters=256, kernel_size=3, strides=(2, 2), activation='relu'),
-            tf.keras.layers.Conv2D(
-                filters=512, kernel_size=3, strides=(2, 2), activation='relu'),
-            tf.keras.layers.Flatten(),
-            # No activation
-            tf.keras.layers.Dense(latent_dim + latent_dim),
-            ]
-            )
+        self.nfilters1 = nfilters1
+        self.nfilters2 = nfilters2
         
-        self.decoder = tf.keras.Sequential(
-            [
-            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-            tf.keras.layers.Dense(units=(patchsize//4)*(patchsize//4)*512, activation=tf.nn.relu),
-            tf.keras.layers.Reshape(target_shape=(patchsize//4, patchsize//4, 512)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=256, kernel_size=3, strides=2, padding='same',
-                activation='relu'),
-            # No activation
-            tf.keras.layers.Conv2DTranspose(
-                filters=ncolors, kernel_size=3, strides=2, padding='same'),
-            ]
-            )
+        self.encoder = nn.Sequential(
+            nn.Conv2d(ncolors, self.nfilters1, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(self.nfilters1, self.nfilters2, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear((patch_size//4)*(patch_size//4)*self.nfilters2, latent_dim + latent_dim),
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, (patch_size//4)*(patch_size//4)*self.nfilters2),
+            nn.Unflatten(1, (self.nfilters2, patch_size//4, patch_size//4)),
+            nn.ReLU(),
+            nn.ConvTranspose2d(self.nfilters2, self.nfilters1, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(self.nfilters1, ncolors, kernel_size=3, stride=2, padding=1, output_padding=1),
+        )
 
-    @tf.function
-    def encode(self, x):
-        mean, logvar = tf.split(self.encoder(x), num_or_size_splits=2, axis=1)
+    def encode(self, x : Tensor):
+        output = self.encoder(x)
+        mean, logvar = torch.split(output, self.latent_dim, dim=1)
         return mean, logvar
 
-    @tf.function
-    def reparameterize(self, mean, logvar):
-        eps = tf.random.normal(shape=tf.shape(mean))
-        return eps * tf.exp(logvar * .5) + mean
+    def reparameterize(self, mean : Tensor, logvar : Tensor):
+        eps = torch.randn_like(mean)
+        return eps * torch.exp(logvar * .5) + mean
 
-    @tf.function
-    def decode(self, z, batch_id, apply_sigmoid=False):
-        logits = self.decoder(tf.concat([z], axis=1))
-        if apply_sigmoid:
-            probs = tf.sigmoid(logits)
-            return probs
-        return logits
-        
-    def call(self, inputs, apply_sigmoid=False):
-        # x = inputs[0]; batch_id = inputs[1]
-        x = inputs; batch_id = None
-        mean, logvar = self.encode(x)
-        z = self.reparameterize(mean, logvar)
-            
-        return self.decode(z, batch_id, apply_sigmoid=apply_sigmoid)
+    def decode(self, z : Tensor):
+        return self.decoder(z)
 
-@tf.function
-def reconstruction_loss(x, x_pred, per_sample=False):
-    logpx_z = -tf.reduce_sum((x_pred - x)**2, axis=[1,2,3])
+def reconstruction_loss(x_true : Tensor, x_pred : Tensor, per_sample: bool=False):
+    sse = torch.sum((x_pred - x_true)**2, dim=(1,2,3))
     
     if per_sample:
-        return -logpx_z
+        return sse
     else:
-        return -tf.reduce_mean(logpx_z)
+        return torch.mean(sse)
 
-#todo: add options to keep latent and to keep reconstruction
-def eval_model(model, x, chunk_size=5000, return_latent=False):
-    rloss = []
-    Z = []
-    for i in range(0, len(x), chunk_size):
-        print('.', end='')
-        chunk = x[i:i+chunk_size]
-        mean, logvar = model.encode(chunk)
-        reconst = model.decode(mean, None)
-        if return_latent:
-            Z.append(mean.numpy())
-        rloss.append(reconstruction_loss(chunk, reconst, per_sample=True).numpy())
-    
-    if return_latent:
-        return np.mean(np.concatenate(rloss)), np.concatenate(Z)
+def kl_loss(mean : Tensor, logvar : Tensor):
+    return -0.5 * torch.mean(
+        torch.sum(1 + logvar - mean.pow(2) - logvar.exp(),
+        dim=1))
+
+def train_one_epoch(model : nn.Module, train_dataset : Dataset,
+        optimizer : torch.optim.Optimizer, scheduler : LRScheduler,
+        batch_size : int, log_interval : int=20, kl_weight : float=1):
+    model.train()
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=torch.Generator(device=torch.get_default_device()))
+    print(f'#batches: {len(train_loader)}')
+
+    losses = []; vaelosses = []; rlosses = []
+    for n, batch in enumerate(train_loader):
+        batch = batch.permute(0, 3, 1, 2)
+
+        # Forward pass
+        mean, logvar = model.encode(batch)
+        z = model.reparameterize(mean, logvar)
+        predictions = model.decode(z)
+
+        rloss = reconstruction_loss(batch, predictions)
+        vaeloss = kl_weight * kl_loss(mean, logvar)
+        loss = vaeloss + rloss
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        optimizer.step()
+
+        # Save info
+        losses.append(float(loss))
+        rlosses.append(float(rloss))
+        vaelosses.append(float(vaeloss))
+
+        # Log
+        if n % log_interval == 0 and n > 0:
+            per_batch_logging(n, rlosses, vaelosses, kl_weight, log_interval, scheduler)
+
+    return pd.DataFrame({'loss':losses, 'rloss':rlosses, 'vaeloss':vaelosses, 'kl_weight':kl_weight})
+
+def evaluate(model : nn.Module, eval_dataset : Dataset, batch_size : int=1000,
+        detailed : bool=False):
+    model.eval()
+    eval_loader = DataLoader(
+        dataset=eval_dataset,
+        batch_size=batch_size,
+        shuffle=False)
+
+    rlosses = []; embeddings = []
+    with torch.no_grad():
+        for n, batch in enumerate(eval_loader):
+            print('.', end='')
+            batch = batch.permute(0, 3, 1, 2)
+            mean, _ = model.encode(batch)
+            predictions = model.decode(mean)
+
+            rlosses.append(
+                reconstruction_loss(batch, predictions, per_sample=True).detach().cpu().numpy()
+                )
+            if detailed: embeddings.append(mean.detach().cpu().numpy())
+
+    if detailed:
+        return np.concatenate(rlosses), np.concatenate(embeddings)
     else:
-        return np.mean(np.concatenate(rloss))
+        return np.concatenate(rlosses).mean()
 
-def train(model, x_train, x_val, on_epoch_end=None, batch_size=256, n_epochs=5,
-            batch_group_size=20, on_batch_group_end=None):
-    from tensorflow.keras.optimizers.schedules import ExponentialDecay
-    lr_schedule = ExponentialDecay(
-        1e-4,
-        decay_steps=len(x_train) // batch_size,
-        decay_rate=0.7, #0.5,
-        staircase=True)
+def per_batch_logging(batch_num : int, rlosses : list, vaelosses : list,
+        kl_weight : float, log_interval : int, scheduler : LRScheduler):
+    lr = scheduler.get_last_lr()[0]
+    cur_rloss = np.mean(rlosses[-log_interval:])
+    cur_vaeloss = np.mean(vaelosses[-log_interval:])
 
-    losses = []; val_losses = []; rlosses = []; val_rlosses = []
-    epochids = []; batchids = []
-    learning_rates = []
-    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, clipvalue=1)
+    print(f'batch {batch_num:5d} | '
+            f'lr {lr:.2g} | '
+            f'r-loss {cur_rloss:.2f} | '
+            f'vae-loss {cur_vaeloss:.2f} | '
+            f'kl-weight {kl_weight}')
 
-    for epoch in range(n_epochs):
-        print(f"Epoch {epoch + 1} of {n_epochs}")
+def simple_per_epoch_logging(model, epoch, epoch_start_time, rlosses, losslog):
+    print(f'end of epoch {epoch}: avg val loss = {rlosses.mean()}')
 
-        # Iterate over batches
-        ix = np.argsort(np.random.randn(len(x_train)))
-        for n, i in enumerate(range(0, len(x_train), batch_size)):
-            x_batch = x_train[ix[i:i+batch_size]]
-            y_batch = x_train[ix[i:i+batch_size]]
+def full_training(model : nn.Module, train_dataset : Dataset,
+        val_dataset : Dataset, optimizer : torch.optim.Optimizer,
+        scheduler : LRScheduler, batch_size : int=128, n_epochs : int=10,
+        kl_weight : float=1, per_epoch_logging=simple_per_epoch_logging):
+    best_val_loss = float('inf')
+    losslogs = []
 
-            # Forward pass
-            with tf.GradientTape() as tape:
-                mean, logvar = model.encode(x_batch)
-                z = model.reparameterize(mean, logvar)
-                kl_loss = -1 * 0.5 * tf.reduce_mean(
-                        tf.reduce_sum(
-                            1 + logvar - tf.square(mean) - tf.exp(logvar),
-                            axis=1
-                        )
-                    )
-                    
-                predictions = model.decode(z, None)
-                rloss = reconstruction_loss(y_batch, predictions)
-                loss = kl_loss + rloss
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
 
-            # Backward pass
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        for epoch in range(1, n_epochs + 1):
+            epoch_start_time = time.time()
+            losslog = train_one_epoch(
+                model, train_dataset, optimizer, scheduler, batch_size, kl_weight=kl_weight)
+            rlosses, _ = evaluate(model, val_dataset, detailed=True)
+            scheduler.step()
 
-            # Save info
-            losses.append(loss.numpy())
-            rlosses.append(rloss.numpy())
-            batchids.append(n)
-            epochids.append(epoch)
-            val_losses.append(np.NaN)
-            val_rlosses.append(np.NaN)
-            learning_rates.append(optimizer.get_config()['learning_rate'])
+            losslog['val_rloss'] = np.NaN
+            losslog.val_rloss.values[-1] = rlosses.mean()
+            losslogs.append(losslog)
+            losslogs_sofar = pd.concat(losslogs, axis=0).reset_index(drop=True)
 
-            if n % batch_group_size == 0:
-                print(f'[{int(rloss.numpy())} , {int(kl_loss.numpy())}]', end=' ')
-                on_batch_group_end(model, epoch+1, n) if on_batch_group_end is not None else None
-        
-        print('end of epoch')
-        val_rlosses[-1] = eval_model(model, x_val)
-        training_meta = pd.DataFrame({'epoch':epochids, 'batch':batchids, 'loss':losses,
-            'rloss':rlosses, 'val_rloss':val_rlosses, 'learning_rate':learning_rates})
-        print(f'est. val rloss = {val_rlosses[-1]}')
+            per_epoch_logging(model, epoch, epoch_start_time, rlosses,
+                losslogs_sofar)
 
-        on_epoch_end(model, epoch+1, training_meta) if on_epoch_end is not None else None
+            if rlosses.mean() < best_val_loss:
+                best_val_loss = rlosses.mean()
+                torch.save(model.state_dict(), best_model_params_path)
 
-    return training_meta
+        model.load_state_dict(torch.load(best_model_params_path)) # load best model states
+    return model, losslogs_sofar
--- vis.py
+++ vis.py
@@ -1,34 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
-def plot(model, epoch, test_sample, sids, show=True, rgb=False, channels=None, pmin=None, pmax=None):
-    predictions = model(test_sample)
+def plot(model, examples, show=True, rgb=False, channels=None, pmin=None, pmax=None):
+    examples = examples.permute(0,3,1,2)
+    model.eval()
+    with torch.no_grad():
+        means, _ = model.encode(examples)
+        predictions = model.decode(means).permute(0,2,3,1).cpu().numpy()
+    examples = examples.permute(0,2,3,1).cpu().numpy()
+    losses = np.mean((examples - predictions)**2, axis=(1,2,3))*examples.shape[1]*examples.shape[2]*examples.shape[3]
 
     if not rgb:
         if channels is None:
             channels = [0,1,2]
         fig = plt.figure(figsize=(32,3*4))
         for j, channel in enumerate(channels):
-            vmin = -np.max(np.abs(test_sample[:,:,:,channel]))
-            vmax = -vmin
-            for i, (a, b) in enumerate(zip(predictions, test_sample)):
-                plt.subplot(6, len(test_sample), len(test_sample)*(2*j) + i + 1)
-                plt.imshow(b[:,:,channel], vmin=vmin, vmax=vmax, cmap='seismic')
+            v = max(abs(pmin[channel]), abs(pmax[channel]))
+            for i, (a, b) in enumerate(zip(predictions, examples)):
+                plt.subplot(6, len(examples), len(examples)*(2*j) + i + 1)
+                plt.imshow(b[:,:,channel], vmin=-v, vmax=v, cmap='seismic')
                 plt.axis('off')
-                plt.subplot(6, len(test_sample), len(test_sample)*(2*j+1) + i + 1)
-                plt.imshow(a[:,:,channel], vmin=vmin, vmax=vmax, cmap='seismic')
+                if j == 0:
+                    plt.text(20, 1, f'{losses[i]:.2f}', ha='center', va='bottom', fontsize=16)
+                plt.subplot(6, len(examples), len(examples)*(2*j+1) + i + 1)
+                plt.imshow(a[:,:,channel], vmin=-v, vmax=v, cmap='seismic')
                 plt.axis('off')
     else:
         if pmin is None:
             pmin = np.array([0,0,0])
         if pmax is None:
             pmax = np.array([1,1,1])
-        fig = plt.figure(figsize=(2*len(test_sample), 4))
-        for i, (a, b) in enumerate(zip(predictions, test_sample)):
-            plt.subplot(2, len(test_sample), i+1)
+        fig = plt.figure(figsize=(2*len(examples), 4))
+        for i, (a, b) in enumerate(zip(predictions, examples)):
+            plt.subplot(2, len(examples), i+1)
             plt.imshow((b - pmin)/(pmax-pmin))
             plt.axis('off')
-            plt.subplot(2, len(test_sample), len(test_sample)+i+1)
+            plt.subplot(2, len(examples), len(examples)+i+1)
             plt.imshow((a - pmin)/(pmax-pmin))
             plt.axis('off')
     plt.tight_layout()
