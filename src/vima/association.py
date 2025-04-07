99130cc92b99a564d94e574ea18df7310bd71468
yakirr <yreshef@broadinstitute.org>
2025-04-07 13:38:44 -0400

    made compatible with new cna

--- src/vima/association.py
+++ src/vima/association.py
@@ -2,14 +2,13 @@ import numpy as np
 import scanpy as sc
 import anndata as ad
 import pandas as pd
-import multianndata as md
 import torch
 from torch.utils.data import DataLoader
 import cna
 from tqdm import tqdm
 pb = lambda x: tqdm(x, ncols=100)
 
-def anndata(patchmeta, Z, samplemeta, var_names=None, use_rep='X', n_comps=10, sampleid='sid'):
+def anndata(patchmeta, Z, var_names=None, use_rep='X', n_comps=10):
     d = ad.AnnData(Z)
     if var_names is not None:
         d.var_names = var_names
@@ -21,13 +20,7 @@ def anndata(patchmeta, Z, samplemeta, var_names=None, use_rep='X', n_comps=10, s
     print('running UMAP')
     sc.pp.neighbors(d, use_rep=use_rep)
     sc.tl.umap(d)
-
-    samplemeta.index = samplemeta.index.astype(str)
-    d.obs.sid = d.obs.sid.astype(str)
-    d = md.MultiAnnData(d)
-    d.samplem = samplemeta
-    d.sampleid = sampleid
-    print(f'built MultiAnnData object with {sampleid} as the unit of analysis')
+    print(f'done')
 
     return d
 
@@ -50,28 +43,26 @@ def apply(model, P, embedding=None, batch_size=1000):
 
     return np.concatenate(Z)
 
-def latentrep(model, P, samplemeta):
+def latentrep(model, P):
     return anndata(P.meta,
-                apply(model, P),
-                samplemeta[samplemeta.index.isin(P.meta.sid.unique())],
-                sampleid='sid')
+                apply(model, P))
 
-def association(d, pheno, fdr=0.1, force_recompute=True, covs=None, Nnull=100000, seed=0, **kwargs):
+def association(d, y, sid_name, batches=None, covs=None, donorids=None, key_added='mncoef',
+                return_full=False, Nnull=10000, fdr=0.1, seed=0, **kwargs):
     if seed is not None: np.random.seed(seed)
-    cna.tl.nam(d, force_recompute=force_recompute)
-    d.samplem['case'] = pheno
-    if covs is not None:
-        d.samplem[covs.columns] = covs
-        
-    res = cna.tl.association(d, d.samplem.case, donorids=d.samplem.donor.values, covs=covs, Nnull=Nnull, **kwargs)
+    
+    res = cna.tl.association(d, y, sid_name, donorids=donorids, covs=covs, batches=batches,
+                             key_added=key_added, Nnull=Nnull, return_full=True, **kwargs)
+    
     print(f'P = {res.p}, used {res.k} MAM-PCs')
-    d.obs['mncoeff'] = res.ncorrs
-    if res.fdrs.fdr.min() <= fdr:
-        print(f'Found {res.fdrs[res.fdrs.fdr < 0.1].iloc[0].num_detected} microniches at FDR {int(fdr*100)}%')
-        d.obs['sig_mncoeff'] = res.ncorrs * (np.abs(res.ncorrs) > res.fdrs[res.fdrs.fdr < fdr].iloc[0].threshold)
-    else:
-        print(f'No microniches found at FDR {int(fdr*100)}%')
-        d.obs['sig_mncoeff'] = 0
-    d.samplem.loc[~np.isnan(d.samplem.case), 'yhat'] = res.yresid_hat
+    
+    mncoef = d.obs[key_added]
+    fdrs = d.obs[f'{key_added}_fdr']
+    nsig = (fdrs <= fdr).sum()
+    print(f'Found {nsig} microniches at FDR {int(fdr*100)}%')
+    d.obs['sig_mncoef'] = mncoef * (fdrs <= fdr)
 
-    return res
+    if return_full:
+        return res
+    else:
+        return res.p
