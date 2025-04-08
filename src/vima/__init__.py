37d696b3328b18da057355f5b5f7624aab8c0cc6
yakirr <yreshef@broadinstitute.org>
2025-04-08 11:00:30 -0400

    top-level access to models

--- src/vima/__init__.py
+++ src/vima/__init__.py
@@ -1,14 +1,15 @@
 from . import data as d
 from . import association as a
 from . import train as t
-from . import ingest as i
+from . import ingest as pp
 from . import vis as v
+from . import models
 
 from .data.patchcollection import PatchCollection
 from .data.samples import read_samples
 from .train.training import train, seed
 from .association import latentrep, association
 
-__all__ = ['d', 'a', 't', 'i', 'v',
+__all__ = ['d', 'a', 't', 'pp', 'v', 'models',
            'PatchCollection', 'read_samples',
            'train', 'latentrep', 'association']
