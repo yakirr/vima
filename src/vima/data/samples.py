dca2c978107693f55c721af9501bcb4cc624d5c0
yakirr <yreshef@broadinstitute.org>
2026-04-23 00:15:41 -0400

    removed fname_parser

--- src/vima/data/samples.py
+++ src/vima/data/samples.py
@@ -8,14 +8,7 @@ import cv2 as cv2
 from tqdm import tqdm
 pb = lambda x: tqdm(x, ncols=100)
 
-def default_parser(fname):
-    fname = os.path.splitext(os.path.basename(fname))[0]
-    return {
-        'donor': fname.split('_')[0],
-        'sid': fname.split('_')[1]
-    }
-
-def read_samples(files, filename_parser, stop_after=None):
+def read_samples(files, stop_after=None):
     if type(files) == str:
         files = glob.glob(files)
     if stop_after is None: stop_after = len(files)
@@ -23,10 +16,19 @@ def read_samples(files, filename_parser, stop_after=None):
     samples = {}
     for f in pb(files[:stop_after]):
         s = xr.open_dataarray(f).astype(np.float32)
-        s.attrs.update(filename_parser(f))
+        s.attrs['sid'] = os.path.splitext(os.path.basename(f))[0]
         samples[s.sid] = s
 
     return samples
+
+def reindex_by_sid(samplemeta, sid_to_donor):
+    # change samplemeta so that each row is a sample rather than a donor
+    sids = sid_to_donor.keys()
+    in_our_data = pd.DataFrame({
+        'sid': sids,
+        'donor': [sid_to_donor[sid] for sid in sids]
+    })
+    return pd.merge(in_our_data, samplemeta, left_on='donor', right_index=True, how='left').set_index('sid', drop=True)
 
 def get_mask(s):
     return (s!=0).any(dim='marker')
