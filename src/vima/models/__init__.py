1262584e5ca02dcaac03d19cf540dbcf9c1357ed
yakirr <yreshef@broadinstitute.org>
2026-04-28 15:15:20 -0400

    added uncompile ability in model load

--- src/vima/models/__init__.py
+++ src/vima/models/__init__.py
@@ -12,7 +12,20 @@ def cVAE(nmarkers, covariate_sizes, nreps=10, compile_mode='default', weights=No
                 ResnetVAE(nmarkers, covariate_sizes)
             for _ in range(nreps)])
     if weights is not None:
-        models.load_state_dict(torch.load(weights), weights_only=True)
+        if torch.cuda.is_available():
+            map_location = None
+            uncompile = False
+        elif torch.backends.mps.is_available():
+            map_location = torch.device('mps')
+            uncompile = True
+        else:
+            map_location = torch.device('cpu')
+            uncompile = True
+        state = torch.load(weights, map_location=map_location, weights_only=True)
+
+        if uncompile:
+            state = {k.replace('._orig_mod', ''): v for k, v in state.items()}
+        models.load_state_dict(state)
     return models
 
 __all__ = ["ResnetVAE", "SimpleVAE"]
